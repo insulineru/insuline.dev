5fe494219f4d6ffc4cfd98a480bc22a436bb0ece
Aleksey Lisun <insuline.ru@gmail.com>
2022-08-13 02:44:08 +0800

    ✨ feat: create base aside component

---
+++ packages/vue-app/src/components/BaseAside.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+defineProps<{ position: 'left' | 'right' }>()
+</script>
+
+<template>
+  <aside class="absolute h-36 w-100vh rotate-90 origin-bottom top-64" :class="position === 'right' ? '-right-64' : '-left-64'">
+    <slot />
+  </aside>
+</template>
