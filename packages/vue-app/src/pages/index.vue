0a9380d1a65e6e1ab0898f521bb06213a3c51028
Aleksey Lisun <insuline.ru@gmail.com>
2022-08-12 15:01:07 +0800

    ✨ feat: intro screen

--- packages/vue-app/src/pages/index.vue
+++ packages/vue-app/src/pages/index.vue
@@ -1,50 +1,12 @@
 <script setup lang="ts">
-const name = $ref('')
 
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
-}
 </script>
 
 <template>
-  <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
-    </div>
-  </div>
+  <main class="h-full w-full flex flex-col justify-end max-w-1200px mx-auto">
+    <h1 class="text-113px leading-none font-bold mb-11">
+      <img src="../assets/images/hand.svg" alt="👋" width="100" height="100" class="inline-block -mt-8">
+      I'm Insuline, <br> a web 3.0 developer
+    </h1>
+  </main>
 </template>
