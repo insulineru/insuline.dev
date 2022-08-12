a49e6ed3abf56b7b5b40e0982394eae82d62b002
Aleksey Lisun <insuline.ru@gmail.com>
2022-08-12 15:00:10 +0800

    ✨ feat: new footer

--- packages/vue-app/src/components/Footer.vue
+++ packages/vue-app/src/components/Footer.vue
@@ -1,17 +1,31 @@
-<template>
-  <nav text-xl mt-6 inline-flex gap-2>
-    <button class="icon-btn !outline-none" @click="toggleDark()">
-      <div v-if="isDark" i-carbon-moon />
-      <div v-else i-carbon-sun />
-    </button>
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
 
-    <a
-      class="icon-btn"
-      i-carbon-logo-github
-      rel="noreferrer"
-      href="https://github.com/antfu/vitesse-lite"
-      target="_blank"
-      title="GitHub"
-    />
+const socials = [{
+  name: 'Github',
+  url: 'https://github.com/insulineru',
+  icon: 'akar-icons:github-fill',
+}, {
+  name: 'Twitter',
+  url: 'https://twitter.com/insulineru',
+  icon: 'akar-icons:twitter-fill',
+}, {
+  name: 'Telegram',
+  url: 'https://facebook.com/insulineru',
+  icon: 'bxl:telegram',
+}]
+</script>
+
+<template>
+  <nav class="pt-10 flex justify-between w-full text-base border-t border-t-hex-393C47 max-w-1200px mx-auto">
+    <ul class="flex gap-4">
+      <li v-for="social in socials" :key="social.name" class="block">
+        <a :href="social.url" class="flex items-center justify-center text-white w-10 h-10 rounded-full p-2 bg-hex-272930 hover:bg-hex-393C47 transition-colors" target="_blank" rel="noopener norefferer">
+          <Icon :icon="social.icon" />
+        </a>
+      </li>
+    </ul>
+    <p>©2022</p>
+    <p>Design by Artem Suslov</p>
   </nav>
 </template>
